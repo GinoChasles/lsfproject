6eb23bc815be2de6403fa189ad41f7f76f4e5c97
ginochasles <ginochasles@gmail.com>
2021-02-26 19:09:31 +0100

    init

---
+++ src/main/java/fr/gino/lsftranslate/LsftranslateApplication.java
@@ -0,0 +1,13 @@
+package fr.gino.lsftranslate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsftranslateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LsftranslateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/gino/lsftranslate/LsftranslateApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gino.lsftranslate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsftranslateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
