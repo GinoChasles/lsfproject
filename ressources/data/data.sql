651d83de258ad6d6be1b704f7569014366642daa
ginochasles <ginochasles@gmail.com>
2021-03-04 16:26:16 +0100

    test bdd non concluant

---
+++ ressources/data/data.sql
@@ -0,0 +1,31 @@
+INSERT INTO genre (nom)
+VALUES ('masculin'),('féminin');
+
+
+INSERT INTO nombre (nom)
+VALUES ('pluriel'),('singulier');
+
+INSERT INTO catGram (nom)
+VALUES
+('ADJ'),
+('ADJ:dem'),
+('ADJ:ind'),
+('ADJ:int'),
+('ADJ:num'),
+('ADJ:pos'),
+('ADV'),
+('ART:def'),
+('ART:inf'),
+('AUX'),
+('CON'),
+('LIA'),
+('NOM'),
+('ONO'),
+('PRE'),
+('PRO:dem'),
+('PRO:ind'),
+('PRO:int'),
+('PRO:per'),
+('PRO:pos'),
+('PRO:rel'),
+('VER');
