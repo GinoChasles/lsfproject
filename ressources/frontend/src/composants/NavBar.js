2c0accb5bd74e19251fca8c65673a111831d4570
GinoChasles <gino.chasles@gmail.com>
2021-03-11 10:03:09 +0100

    navbar responsive

--- ressources/frontend/src/composants/NavBar.js
+++ ressources/frontend/src/composants/NavBar.js
@@ -14,7 +14,7 @@ export default class NavBar extends Component {
                             </li>
                              <li><img id="logo-nav" src={process.env.PUBLIC_URL + '/img/logo1.png'} alt="logo lsf4all" /></li>
                             <li>
-                                <a className="nav-links" href="#">Mon compte</a>
+                                <a className="nav-links" href="#">MonCompte</a>
                             </li>
                 </ul>
             </div>
