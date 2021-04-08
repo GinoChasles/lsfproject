da4ba0398848a658f08a248a9ae5e98c1caa7212
ginochasles <ginochasles@gmail.com>
2021-04-08 22:16:44 +0200

    08/04/21

--- ressources/frontend/src/composants/Infos.js
+++ ressources/frontend/src/composants/Infos.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 
 export default function Infos(props) {
     const donnees = [props.catgram.name,props.infover,props.nbletters,props.nbsyll,props.reverspel]
-    const [infos, setInfos] = useState(donnees);
+    const [infos, setInfos] = useState('');
 
     return (
         <article>
-            {infos}
+            Catgram : {infos.catgram.name}
+            Infos verbales : {infos.infover}
             <button onClick={()=> setInfos(props)}></button>
         </article>
     )
